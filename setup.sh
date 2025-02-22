584f7b360b7e19d7d1f1ef27a2456fe5b46d8da9
gameshler <jabrabasel2000@gmail.com>
2025-02-22 21:58:56 +0200

    ref: setup.sh

--- setup.sh
+++ setup.sh
@@ -27,4 +27,28 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 nvm install 20
 nvm use 20
 npm install -g lite-server http-server license gitignore
+echo "Downloading Microsoft Office Installer packages..."
+
+# URL for Microsoft_365_and_Office_Installer and Microsoft Office Serializer
+OFFICE_INSTALLER_URL="https://mega.nz/file/PNNlWKCa#vBSY-AGuPyXB-qVMZwoSWg_cPd3o2w0008YF6fXNrTw"
+OFFICE_SERIALIZER_URL="https://mega.nz/file/PAsTnRJD#JUIWkULr5tHN4VzE-v1iigHCFWAHtYHaZ52Cs0AZUeE"
+
+# Use curl to download the Office Installer and Serializer
+curl -L -o ~/Downloads/Microsoft_365_and_Office_Installer.pkg $OFFICE_INSTALLER_URL
+curl -L -o ~/Downloads/microsoft_office_ltsc_2024_vl_serializer.pkg $OFFICE_SERIALIZER_URL
+
+# Install Microsoft 365 Office using the downloaded packages
+echo "Installing Microsoft 365 Office..."
+sudo installer -pkg ~/Downloads/Microsoft_365_and_Office_Installer.pkg -target /
+
+# Install Microsoft Office Serializer (activation)
+echo "Installing Microsoft Office Serializer..."
+sudo installer -pkg ~/Downloads/microsoft_office_ltsc_2024_vl_serializer.pkg -target /
+
+# Cleanup the downloaded files
+echo "Cleaning up..."
+rm ~/Downloads/Microsoft_365_and_Office_Installer.pkg
+rm ~/Downloads/microsoft_office_ltsc_2024_vl_serializer.pkg
+
+echo "Office installation completed successfully!"
 echo "Setup completed successfully!"
