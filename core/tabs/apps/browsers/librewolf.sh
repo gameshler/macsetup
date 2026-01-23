5999efdb2211cc7c13c706ab800341817a3355b5
unknown <baddour.jb@gmail.com>
2026-01-23 14:59:35 +0200

    feat: added more apps

---
+++ core/tabs/apps/browsers/librewolf.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+. "$COMMON_SCRIPT"
+
+installLibreWolf() {
+    if ! brew_program_exists librewolf; then
+        printf "Installing LibreWolf..."
+        brew install --cask librewolf
+        if [ $? -ne 0 ]; then
+            printf "Failed to install LibreWolf Browser. Please check your Homebrew installation."
+            exit 1
+        fi
+        printf "LibreWolf Browser installed successfully!"
+    else
+        printf "LibreWolf Browser is already installed."
+    fi
+}
+
+checkPackageManager
+installLibreWolf
---
+++ core/tabs/apps/developer-tools/cursor.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+. "$COMMON_SCRIPT"
+
+installCursor() {
+    if ! brew_program_exists cursor; then
+        printf "Installing Cursor..."
+        brew install --cask cursor
+        if [ $? -ne 0 ]; then
+            printf "Failed to install Cursor. Please check your Homebrew installation."
+            exit 1
+        fi
+        printf "Cursor installed successfully!"
+    else
+        printf "Cursor is already installed."
+    fi
+}
+
+checkPackageManager
+installCursor
