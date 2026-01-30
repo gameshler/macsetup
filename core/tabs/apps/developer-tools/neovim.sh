0e11213662a39532a65ea8e306d71667b48e9b0f
gameshler <baddour.jb@gmail.com>
2026-01-30 05:21:18 +0200

    feat: neovim.sh

---
+++ core/tabs/apps/developer-tools/neovim.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -e
+
+. "$COMMON_SCRIPT"
+
+cloneNeovim() {
+    if [ -z "${TEMP_DIR:-}" ] || [ ! -d "$TEMP_DIR" ]; then
+        printf "Missing or Invalid Temp Directory\n" >&2
+        exit 1
+    fi
+
+    git clone https://github.com/gameshler/neovim.git "$TEMP_DIR"
+
+}
+
+installNeovim() {
+    if ! brew_program_exists neovim ripgrep; then
+        printf "Installing Neovim..."
+        brew install neovim ripgrep shellcheck
+        if [ $? -ne 0 ]; then
+            printf "Failed to install Neovim. Please check your Homebrew installation."
+            exit 1
+        fi
+        printf "Neovim installed successfully!"
+    else
+        printf "Neovim already installed."
+    fi
+}
+
+linkNeovimConfig() {
+    printf "Linking Neovim Configuration Files..."
+    mkdir -p "$HOME/.config/nvim"
+    ln -s "$TEMP_DIR/lua" "$HOME/.config/nvim/"
+    ln -s "$TEMP_DIR/init.lua/" "$HOME/.config/nvim/"
+    ln -s "$TEMP_DIR/lazy-lock.json" "$HOME/.config/nvim/"
+
+}
+
+checkPackageManager
+installNeovim
+cloneNeovim
+linkNeovimConfig
