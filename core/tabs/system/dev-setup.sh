8af09ac9691fea75006c94bc7c71a5f40c45b104
Gameshler <84982733+gameshler@users.noreply.github.com>
2026-08-23 01:43:19 +0300

    Update dev-setup.sh

--- core/tabs/system/dev-setup.sh
+++ core/tabs/system/dev-setup.sh
@@ -32,7 +32,7 @@ setup_config() {
 install_nvm() {
     if ! command_exists "nvm"; then
         printf "%b\n" "Installing NVM..."
-        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.7/install.sh | bash
 
         export NVM_DIR="$HOME/.nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
